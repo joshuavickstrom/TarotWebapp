ea09b9960bdeedac7827dd3cdf55e2dd533e3da0
Lili Clift <liliclift@gmail.com>
2024-03-23 15:17:41 -0700

    Getting current moon phase

--- esoteria/src/App.vue
+++ esoteria/src/App.vue
@@ -2,6 +2,7 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 import OpenAI from "openai";
+import axios from "axios";
 </script>
 
 <template>
@@ -26,12 +27,15 @@ import OpenAI from "openai";
     data() {
       return {
         openai: null,
+        authString: btoa(`992aba9d-c7d8-41ee-abdd-c8de26cc8e29:${import.meta.env.VITE_ASTRO_AI}`),
+        moonPhase: null,
       }
     },
     mounted() {
-      console.log(import.meta.env.VITE_OPENAI_API_KEY)
-      this.openai = new OpenAI({ apiKey: import.meta.env.VITE_OPENAI_API_KEY });
-      this.main();
+      // commenting out openAI stuff until we figure out server stuff
+      // this.openai = new OpenAI({ apiKey: import.meta.env.VITE_OPENAI_API_KEY });
+      // this.main();
+      this.GetCurrentMoon();
     },
     methods: {
       async main() {
@@ -41,6 +45,20 @@ import OpenAI from "openai";
         });
 
         console.log(completion.choices[0]);
+      },
+      GetCurrentMoon() {
+        let yourDate = new Date();
+        const offset = yourDate.getTimezoneOffset();
+        yourDate = new Date(yourDate.getTime() - (offset*60*1000));
+        const formattedDate = yourDate.toISOString().split('T')[0];
+        axios.get(`https://api.astronomyapi.com/api/v2/bodies/positions/moon?latitude=41.85003&longitude=-87.65005&elevation=597&from_date=${formattedDate}&to_date=${formattedDate}&time=15%3A01%3A28`, {
+          headers: {
+            Authorization: `Basic ${this.authString}`
+          },
+        }).then((res) => {
+          this.moonPhase = res.data.data.table.rows[0].cells[0].extraInfo.phase.string;
+          console.log(this.moonPhase)
+        }).catch((err) => console.error(err));
       }
     }
   }
